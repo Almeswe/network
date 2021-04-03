a0e3ed62325db6ed5bb7cf131cc34bbfba1022ea
Almeswe <52577119+Almeswe@users.noreply.github.com>
2021-04-03 13:17:27 +0300

    Update main.cpp

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,6 +3,8 @@ using namespace Network;
 
 int main(char** argv, int argc)
 {
+	//"C\\Users\\Almes\\source\\repos\\network\\network\\server\\Debug\\server.exe"
+	//"C:\Users\Almes\source\repos\network\network\server\Debug\server.exe"
 	Client* c = new Client("27015","localhost");
 	c->Connect();
 }
