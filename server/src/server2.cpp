6480eb4cf524ff2e0a931c910401fe0a9cb21e21
Almeswe <52577119+Almeswe@users.noreply.github.com>
2021-04-07 00:07:49 +0300

    added working server (only with PuTTy client)

--- server/src/server2.cpp
+++ server/src/server2.cpp
@@ -1,13 +1,8 @@
 #include "server2.h"
 
-#define Ok   0
-#define Fail 1
-
-#define CLIENT_DISCONNECTED (this->receivedBytes <= 0)
-
 namespace Network
 {
-	Server2::Server2(int port,std::string ip) : port(port),ip(ip),threadCount(0),receivedBytes(0)
+	Server2::Server2(int port,std::string ip) : port(port),ip(ip)
 	{
 		Infolog("Initializing a server...");
 		if (this->Init())
@@ -61,17 +56,30 @@ namespace Network
 		return Ok;
 	}
 
-	SOCKET Server2::AcceptToServerSocket(SOCKET serversocket, sockaddr* clientaddr, int* clientaddrsize)
+	int Server2::AcceptClient(SOCKET serverSocket, ConnectedClient* client)
 	{
-		SOCKET sckt = accept(serversocket, clientaddr, clientaddrsize);
-		if (sckt != INVALID_SOCKET)
+		client->addrSize = sizeof(client->addr);
+		client->socket = accept(serverSocket, (sockaddr*)&client->addr, &client->addrSize);
+		if (client->socket != INVALID_SOCKET)
 		{
-			ZeroMemory(this->host, NI_MAXHOST);
-			ZeroMemory(this->service, NI_MAXSERV);
-			if (!getnameinfo((sockaddr*)&this->clientAddr, this->clientAddrSize, this->host, NI_MAXHOST, this->service, NI_MAXSERV, 0))
-				Log("Client " + std::string(this->host)+ " as Thread " + std::to_string(this->threadCount+1) + " connected.","CONNECTION");
+			ZeroMemory(client->host, NI_MAXHOST);
+			ZeroMemory(client->service, NI_MAXSERV);
+			if (!getnameinfo((sockaddr*)&client->addr, client->addrSize, client->host, NI_MAXHOST, client->service, NI_MAXSERV, 0))
+				Log("Client " + std::string(client->host) + " as " + std::to_string(client->socket) + " connected.", "CONNECTION");
+			SendFromServerTo(client, "You've entered the chatroom as: " + std::to_string(client->socket) + "\r\n");
+			if (this->clients.size() > 0)
+			{
+				std::string parts = "Clients in this chatroom: \r\n";
+				for (ConnectedClient* connectedClient : this->clients)
+					parts += "   |" + std::to_string(connectedClient->socket) + "\r\n";
+				SendFromServerTo(client, parts);
+			}
+			else
+				SendFromServerTo(client, "This chatroom is empty right now.");
+			SendFromServerToAll(client, "Client " + std::string(client->host) + " as " + std::to_string(client->socket) + " connected.");
+			return Ok;
 		}
-		return sckt;
+		return Fail;
 	}
 
 	void Server2::StartListening()
@@ -82,34 +90,144 @@ namespace Network
 		Empty;
 		while(true)
 		{
-			this->clientAddrSize = sizeof(this->clientAddr);
-			this->clientSocket = AcceptToServerSocket(this->serverSocket, (sockaddr*)&this->clientAddr, &this->clientAddrSize);
-			this->activeThreads.push_back(std::thread(&Server2::ExchangeData, this, this->clientSocket));
-			this->activeThreads[this->activeThreads.size() - 1].detach();
+			ConnectedClient* client = new ConnectedClient();
+			if (this->AcceptClient(this->serverSocket, client))
+				continue;
+			this->clients.push_back(client);
+			this->activeThreads.push_back(std::thread(&Server2::ExchangeDataWith, this, client));
+			this->activeThreads[this->activeThreads.size()-1].detach();
 		}
 	}
 
-	void Server2::ExchangeData(SOCKET clientsocket)
+	void Server2::StartListening2()
 	{
-		int received = 0;
-		
-		this->threadCount++;
-		int currthread = this->threadCount;
+		this->ListenServerSocket(this->serverSocket, SOMAXCONN);
+		Empty;
+		Infolog("Waiting for connections...");
+		Empty;
 
-		char buffer[TcpPackSize];
+		fd_set master;
+		FD_ZERO(&master);
+		FD_SET(this->serverSocket,&master);
+
+		while (true)
+		{
+			fd_set copy = master;
+			int socketCount = select(0, &copy, nullptr, nullptr, nullptr);
+			
+			for (size_t i = 0; i < socketCount; i++)
+			{
+				if (this->serverSocket == copy.fd_array[i])
+				{
+					ConnectedClient* client = new ConnectedClient();
+					if (this->AcceptClient(this->serverSocket, client))
+						break;
+					this->clients.push_back(client);
+					FD_SET(client->socket, &master);
+				}
+				else
+				{
+					ConnectedClient* listeningClient = this->GetClientWithSocket(copy.fd_array[i]);
+					if (listeningClient == nullptr || this->ReceiveData(listeningClient) < 0)
+						break;
+					this->ProcessData(listeningClient);
+					for (SOCKET nonListeningSocket : master.fd_array)
+					{
+						ConnectedClient* nonListeningClient = this->GetClientWithSocket(nonListeningSocket);
+						if (nonListeningClient != nullptr)
+							if (nonListeningClient->socket != this->serverSocket && nonListeningClient->socket != copy.fd_array[i])
+								this->SendDataTo(nonListeningClient, listeningClient->buffer, std::to_string(nonListeningClient->socket).c_str());
+					}
+				}
+			}
+		}
+	}
+	
+	ConnectedClient* Server2::GetClientWithSocket(SOCKET socket)
+	{
+		for (ConnectedClient* client : this->clients)
+			if (client->socket == socket)
+				return client;
+		return nullptr;
+	}
+
+	void Server2::ExchangeDataWith(ConnectedClient* client)
+	{
 		do
 		{
-			ZeroMemory(buffer, TcpPackSize);
-			received = recv(clientsocket, buffer, TcpPackSize, 0);
-			if (received > 0)
+			if (this->ReceiveData(client) < 0)
+				break;
+			this->ProcessData(client);
+			SendFromClientToAll(client, client->buffer, std::to_string(client->socket).c_str());
+		}
+		while (client->bufSize > 0);
+		if (this->DisconnectClient(client))
+			Errorlog("Client " + std::to_string(client->socket) + " already disconnected.");
+		else
+		{
+			Log("Client " + std::to_string(client->socket) + " disconnected.", "DISCONNECTION");
+			SendFromServerToAll(client, "Client " + std::to_string(client->socket) + " disconnected.");
+		}
+	}
+
+	int Server2::DisconnectClient(ConnectedClient* client)
+	{
+		for (size_t i = 0; i < this->clients.size(); i++)
+		{
+			if (client->socket == clients[i]->socket)
 			{
-				Log(std::string(buffer), "Thread " + std::to_string(currthread)); // processing 
-				send(clientsocket, buffer, TcpPackSize, 0);
+				closesocket(client->socket);
+				this->clients.erase(this->clients.begin() + i);
+				return Ok;
 			}
-		} 
-		while (received > 0);
-		Log("Thread "+ std::to_string(currthread) +" is closed. Client disconnected.","DISCONNECTION");
-		this->threadCount--;
+		}
+		return Fail;
+	}
+
+	int Server2::ReceiveData(ConnectedClient* client)
+	{
+		ZeroMemory(client->buffer, TcpPackSize);
+		client->bufSize = recv(client->socket, client->buffer, TcpPackSize, 0);
+		return client->bufSize;
+	}
+
+	int Server2::ProcessData(ConnectedClient* client)
+	{
+		if (client->bufSize > 0)
+			Log(std::string(client->buffer), std::to_string(client->socket));
+		return Ok;
+	}
+
+	int Server2::SendDataTo(ConnectedClient* client, std::string buffer, std::string from)
+	{
+		return this->SendDataTo(client, buffer.c_str(), std::string(from).c_str()) ? Fail : Ok;
+	}
+
+	int Server2::SendDataTo(ConnectedClient* client, const char* buffer, const char* from)
+	{
+		if (this->IsEmptyMessage(buffer))
+			return Ok;
+		std::string message = std::string(from) + ": " + std::string(buffer) + "\r\n";
+		if (send(client->socket, message.c_str(), message.size(), 0) == SOCKET_ERROR)
+		{
+			Errorlog("Can't send message to " + std::to_string(client->socket));
+			return Fail;
+		}
+		else
+			return Ok;
+	}
+
+	int Server2::SendDataTo(std::vector<ConnectedClient*> clients, ConnectedClient* exceptTo, const char* buffer, const char* from)
+	{
+		for (ConnectedClient* client : clients)
+			if (client != exceptTo)
+				this->SendDataTo(client, buffer, from);
+		return Ok;
+	}
+
+	int Server2::SendDataTo(std::vector<ConnectedClient*> clients, ConnectedClient* exceptTo, std::string buffer, std::string from)
+	{
+		return this->SendDataTo(clients, exceptTo, buffer.c_str(), from.c_str()) ? Fail : Ok;
 	}
 
 	void Server2::Run()
@@ -121,6 +239,14 @@ namespace Network
 	void Server2::Stop()
 	{
 		this->~Server2();
+	}
+
+	bool IsEmptyMessage(const char* buffer)
+	{
+		for (size_t i = 0; i < std::strlen(buffer); i++)
+			if (!std::iscntrl(buffer[i]) && buffer[i] != ' ')
+				return false;
+		return true;
 	}
 
 	void Server2::ReportError(std::string msg, int type)
@@ -138,6 +264,8 @@ namespace Network
 	Server2::~Server2()
 	{
 		this->activeThreads.clear();
+		DisconnectAllClients;
+		this->clients.clear();
 		closesocket(this->serverSocket);
 		WSACleanup();
 	}
--- server/src/server2.h
+++ server/src/server2.h
@@ -3,9 +3,12 @@
 
 #include <vector>
 #include <thread>
+#include <string>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
+#define Ok   0
+#define Fail 1
 #define TcpPackSize 512
 
 #define Empty	      Console::Informator::Empty();
@@ -17,33 +20,41 @@
 #define Error(msg) this->ReportError(msg,0)
 #define Success    Infolog("Success.")
 
+#define SendFromServerTo(client, msg)	 this->SendDataTo(client, msg, "SERVER")
+#define SendFromServerToAll(except, msg) this->SendDataTo(this->clients, except, msg, "SERVER")
+#define SendFromClientToAll(except, msg, from) this->SendDataTo(this->clients, except, msg, from)
+
+#define DisconnectAllClients for(ConnectedClient* client : this->clients) this->DisconnectClient(client)
+
 namespace Network
 {
-
-	class Server2
+	class ConnectedClient
 	{
-		private:
-			int	port;
-			int	threadCount;
-			int	clientCount;
-			int	receivedBytes;
-			int	serverAddrSize;
-			int	clientAddrSize;
+		public:
+			int	bufSize;
+			int	addrSize;
 
 			char host[NI_MAXHOST];
 			char service[NI_MAXSERV];
 			char buffer[TcpPackSize];
 
-			SOCKET		serverSocket;
-			SOCKET		clientSocket;
+			SOCKET		socket;
+			sockaddr_in addr;
+	};
+
+	class Server2
+	{
+		private:
+			int	port;
+			int	serverAddrSize;
 
 			WSADATA		wsa;
+			SOCKET		serverSocket;
 			sockaddr_in serverAddr;
-			sockaddr_in clientAddr;
 
-			std::string ip;
-			std::vector<std::thread> activeThreads;
-			std::vector<ConnectedClient> clients;
+			std::string					  ip;
+			std::vector<std::thread>	  activeThreads;
+			std::vector<ConnectedClient*> clients;
 
 		public:
 			Server2(int port, std::string ip);
@@ -61,18 +72,25 @@ namespace Network
 			int BindServerSocket(SOCKET socket, sockaddr* addr, int addrsize);
 
 			SOCKET CreateServerSocket(int af, int type, int protocol);
-			SOCKET AcceptToServerSocket(SOCKET serversocket, sockaddr* clientaddr, int* clientaddrsize);
+
+			int AcceptClient(SOCKET serverSocket, ConnectedClient* client);
 
 		private:
 			int Init();
 			void StartListening();
+			void StartListening2();
+
+			ConnectedClient* GetClientWithSocket(SOCKET socket);
+			int DisconnectClient(ConnectedClient* client);
 
 		private:
-			void ExchangeData(SOCKET clientsocket);
-
-			int SendData(SOCKET clientsocket, char* buffer);
-			int ProcessData(char* buffer, int bufsize);
-			int ReceiveData(SOCKET clientsocket, char* buffer, int bufsize);
+			int ReceiveData(ConnectedClient* client);
+			int ProcessData(ConnectedClient* client);
+			void ExchangeDataWith(ConnectedClient* client);
+			int SendDataTo(ConnectedClient* client, std::string buffer, std::string from);
+			int SendDataTo(ConnectedClient* client, const char* buffer, const char* from);
+			int SendDataTo(std::vector<ConnectedClient*> clients, ConnectedClient* exceptTo, const char* buffer, const char* from);
+			int SendDataTo(std::vector<ConnectedClient*> clients, ConnectedClient* exceptTo, std::string buffer, std::string from);
 
 		private:
 			void ReportError(std::string msg, int type);
