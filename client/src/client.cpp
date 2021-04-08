d8a31b68e307bcfbf679e1b0b75b38c809150a18
Almeswe <52577119+Almeswe@users.noreply.github.com>
2021-04-09 02:59:43 +0300

    new client

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,45 +1,93 @@
 #include "client.h"
-
+#include "server.cpp"
 
 namespace Network
 {
+	int Client::Init()
 	{
-		Log("Initializing...");
-		this->port = port;
-		this->addresHints.ai_family = AF_INET;
-		this->addresHints.ai_flags = AI_PASSIVE;
-		this->addresHints.ai_socktype = SOCK_STREAM;
-		this->addresHints.ai_protocol = IPPROTO_TCP;
-
+		if (WSAStartup(MAKEWORD(2, 2), &this->wsa))
+			return Fail;
+		return Ok;
 	}
 
+	Client::Client()
 	{
+		Infolog("Initializing a client...");
+		if (this->Init())
+			Panic("Initializing is failed.");
+		Infolog("Initialization finished successfully.");
+		Seprtr;
+	}
+
+	void Client::Connect(int port, std::string ip)
+	{
+		if (EmptyIP != ip && EmptyPort != port)
+			closesocket(this->clientSocket);
+		this->ip = ip;
+		this->port = port;
+		this->clientAddrSize = sizeof(this->clientAddr);
+		CreateAddressHints(AF_INET, this->port, this->ip, &this->clientAddr);
+		this->clientSocket = CreateSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (this->clientSocket == INVALID_SOCKET)
+			Panic("Client socket creation failed.");
+		Infolog("Connecting to server (port:" + std::to_string(port) + ", ip:" + ip + ") ...");
+		if (connect(this->clientSocket, (sockaddr*)&this->clientAddr, this->clientAddrSize))
+			Panic("Connecting is failed.");
+		Infolog("Connected successfully.");
+		this->ExchangeData();
+	}
+
+	void Client::ExchangeData()
+	{
+		do
 		{
+			if (this->ReceiveData())
+				break;
+			this->ProcessData();
 		}
+		while (this->receivedLen > 0);
+		this->Disconnect();
 	}
 	
 	int Client::SendDataTo()
 	{
+		std::string msg;
 		printf("Enter message: ");
+		std::getline(std::cin, msg);
+		if (send(this->clientSocket, msg.c_str(), msg.size(), 0) == SOCKET_ERROR)
 		{
-			return 1;
-		}
-		return 0;
-	}
-	int Client::ReceiveData()
-	{
-		{
+			Panic("Sending data failed.");
 			return 1;
 		}
 		return 0;
 	}
 	
+	int Client::ReceiveData()
 	{
+		if ((this->receivedLen = recv(this->clientSocket, this->buffer, TcpPackSize, 0)) == SOCKET_ERROR)
 		{
+			Panic("Error when receiving data.");
+			return 1;
 		}
+		return 0;
 	}
+	
+	int Client::ProcessData()
+	{
+		if (this->receivedLen > 0 && !IsEmptyMessage(this->buffer))
+		{
+			std::string received = " ";
+			for (size_t i = 0; i < this->receivedLen; i++)
+				received += this->buffer[i];
+			Printc(received);
+		}
+		return Ok;
+	}
+	
 	void Client::Disconnect()
 	{
+		closesocket(this->clientSocket);
+		//??
 		WSACleanup();
 	}
 }
--- client/src/client.h
+++ client/src/client.h
@@ -1,8 +1,44 @@
+#pragma once
 #pragma warning(disable : 4996)
+#include "server.h"
 
+#define EmptyIP   ""
+#define EmptyPort -1
 
 namespace Network
 {
 	class Client
 	{
+		private:
+			int port = EmptyPort;
+			int receivedLen;
+			int clientAddrSize;
+
+			WSADATA wsa;
+
+			SOCKET clientSocket;
+			sockaddr_in clientAddr;
+
+			char buffer[TcpPackSize];
+			std::string ip = EmptyIP;
+			//char host[]
+			//char service
+
+		public:
+			Client();
+			~Client();
+
+		public:
+			int Init();
+
+		public:
+			void Connect(int port, std::string ip);
+			void Disconnect();
+
+		private:
+			int ReceiveData();
+			int SendDataTo();
+			int ProcessData();
+			void ExchangeData();
 	};
+}
