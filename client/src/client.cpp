db63d5f3bb5c4fdc9340bc59be1d754a8924fc90
Almeswe <52577119+Almeswe@users.noreply.github.com>
2021-04-09 02:59:31 +0300

    old code removed

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,12 +1,8 @@
 #include "client.h"
 
-#define Log(msg) this->InfoLog(msg)
-#define SuccessLog Log("Success!")
-#define Error(msg,fn) this->ErrorLog(msg,fn)
 
 namespace Network
 {
-	Client::Client(string port,string hostname)
 	{
 		Log("Initializing...");
 		this->port = port;
@@ -15,77 +11,35 @@ namespace Network
 		this->addresHints.ai_socktype = SOCK_STREAM;
 		this->addresHints.ai_protocol = IPPROTO_TCP;
 
-		//Initialize Winsock 
-		if (WSAStartup(MAKEWORD(2, 2), &this->wsadata))
-			Error("WSAStartup failed", "Client::Client()");
-		else if (getaddrinfo(hostname.c_str(), DEFAULT_ANSI_PORT, &this->addresHints, &this->addressResult))
-			Error("GetAddressInfo failed", "Client::Client()");
-		else if ((this->connectSocket = socket(this->addressResult->ai_family, this->addressResult->ai_socktype, this->addressResult->ai_protocol)) == SOCKET_ERROR)
-			Error("Connect socket creation failed", "Client::Client()");
-		SuccessLog;
 	}
 
-	void Client::Connect()
 	{
-		Log("Connecting to server...");
-		if(connect(this->connectSocket, this->addressResult->ai_addr, this->addressResult->ai_addrlen) == SOCKET_ERROR)
 		{
-			closesocket(this->connectSocket);
-			Error("Connecting failed","Client::Connect()");
-		}
-		else
-		{
-			SuccessLog;
-			freeaddrinfo(this->addressResult);
-			for (;;)
-			{
-				if (this->SendDataTo())
-					break;
-				if (this->ReceiveData())
-					break;
-				this->ProcessData();
-			}
-			this->Disconnect();
 		}
 	}
-
+	
 	int Client::SendDataTo()
 	{
-		string msg;
 		printf("Enter message: ");
-		getline(cin, msg);
-		if (send(this->connectSocket, msg.c_str(), msg.size(), 0) == SOCKET_ERROR)
 		{
-			Error("Sending data failed", "Client::SendDataTo()");
 			return 1;
 		}
 		return 0;
 	}
-
 	int Client::ReceiveData()
 	{
-		if ((this->receivedLen = recv(this->connectSocket, this->buffer, TCP_DATA_PACK, 0)) == SOCKET_ERROR)
 		{
-			Error("Error when receiving data", "Client::ReceiveData");
 			return 1;
 		}
 		return 0;
 	}
 	
-	void Client::ProcessData()
 	{
-		string receivedstr = "";
-		for (size_t i = 0; i < this->receivedLen; i++)
 		{
-			receivedstr += this->buffer[i];
-			this->buffer[i] += 1;
 		}
-		Log("RECEIVED DATA FROM SERVER: " + receivedstr);
 	}
-
 	void Client::Disconnect()
 	{
-		closesocket(this->connectSocket);
 		WSACleanup();
 	}
 }
